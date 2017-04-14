c38099f962d2b5547ebaea0054c68a23e68b21a6
IncredibleQuark <lkupinski@gmail.com>
2017-04-14 13:32:48 +0200

    Created folder structure

--- index.php
+++ index.php
@@ -4,3 +4,19 @@
  echo "witam państwa";
 
  ?>
+
+<html lang="en">
+    <head>
+        <meta charset="utf-8">
+        <meta http-equiv="X-UA-Compatible" content="IE-Edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1"> <!--responsive web design, adapting to different devices -->
+        <title>OnlineShop</title>
+        
+       
+    </head>
+
+    <body>
+                
+    </body>
+
+</html>
---
+++ resources/config.php
@@ -0,0 +1,5 @@
+<?php
+
+//  polaczenie z baza
+ 
+
