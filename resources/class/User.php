eb2016416ea9d2d829745e1ea4a6fe1bfb0f9e0f
IncredibleQuark <lkupinski@gmail.com>
2017-04-16 00:29:04 +0200

    Class User finished

--- resources/class/User.php
+++ resources/class/User.php
@@ -1,10 +1,140 @@
 <?php
 
-
 class User extends activeRecord {
-    
-    
+
+    private $name;
+    private $surname;
+    private $email;
+    private $passwordHash;
+
+    public function __construct() {
+        parent::__construct();
+        $this->name = '';
+        $this->surname = '';
+        $this->email = '';
+        $this->passwordHash = '';
+    }
+
+    public function getId() {
+        return $this->id;
+    }
+
+    function getName() {
+        return $this->name;
+    }
+
+    function getSurname() {
+        return $this->surname;
+    }
+
+    function getEmail() {
+        return $this->email;
+    }
+
+    function getPassword() {
+        return $this->passwordHash;
+    }
+
+    function setName($name) {
+        $this->name = $name;
+    }
+
+    function setSurname($surname) {
+        $this->surname = $surname;
+    }
+
+    function setEmail($email) {
+        $this->email = $email;
+    }
+
+    function setPassword($passwordHash) {
+        $this->passwordHash = $passwordHash;
+    }
+
+    //creating new user and updating user that already exists
+    public function save() {
+        if (self::$db->conn != null) {
+            if ($this->id == -1) {
+                $sql = "INSERT INTO User (name, surname, email, passwordHash) values (:name, :surname, :email, :passwordHash)";
+                $stmt = self::$db->conn->prepare($sql); //MySQL inejction preventing during registering new user
+                $result = $stmt->execute([
+                    'name' => $this->name,
+                    'surname' => $this->surname,
+                    'email' => $this->email,
+                    'passwordHash' => $this->passwordHash
+                ]);
+
+                if ($result == true) {
+                    $this->id = self::$db->conn->lastInsertId();
+                    return true;
+                } else {
+                    echo self::$db->conn->error;
+                }
+            } else {
+                $sql = "UPDATE User SET name = :name, surname = :surname, email = :email, passwordHash = :passwordHash WHERE id = $this->id";
+
+                $stmt = self::$db->conn->prepare($sql);
+                $result = $stmt->execute([
+                    'name' => $this->name,
+                    'surname' => $this->surname,
+                    'email' => $this->email,
+                    'passwordHash' => $this->passwordHash
+                ]);
+
+                if ($result == true) {
+                    return true;
+                }
+            }
+        } else {
+            echo "Brak polaczenia\n";
+        }
+        return false;
+    }
+
+    //delete function avalible only for admin users
+    public function delete() {
+        if ($this->id != -1) {
+            if (self::$db->conn->query("DELETE FROM User WHERE id=$this->id")) {
+                $this->id = -1;
+                return true;
+            }
+            return false;
+        }
+        return true;
+    }
+
+    static public function loadAll() {
+        self::connect();
+        $sql = "SELECT * FROM User";
+        $returnTable = [];
+        if ($result = self::$db->conn->query($sql)) {
+            foreach ($result as $row) {
+                $loadedUser = new User();
+                $loadedUser->id = $row['id'];
+                $loadedUser->name = $row['name'];
+                $loadedUser->surname = $row['surname'];
+                $loadedUser->passwordHash = $row['passwordHash'];
+                $loadedUser->email = $row['email'];
+                $returnTable[] = $loadedUser;
+            }
+        }
+        return $returnTable;
+    }
+
+    static public function loadById($id) {
+        self::connect();
+        $sql = "SELECT * FROM User WHERE id=$id";
+        $result = self::$db->conn->query($sql);
+        if ($result && $result->rowCount() == 1) {
+            $row = $result->fetch(PDO::FETCH_ASSOC);
+            $loadedUser = new User();
+            $loadedUser->id = $row['id'];
+            $loadedUser->name = $row['name'];
+            $loadedUser->surname = $row['surname'];
+            $loadedUser->passwordHash = $row['passwordHash'];
+            $loadedUser->email = $row['email'];
+            return $loadedUser;
+        }
+        return null;
+    }
 }
-
-
-
