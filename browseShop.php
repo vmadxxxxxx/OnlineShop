b31be71a621d4d7afb541a4c158247386ba2e5bd
Marek Bazylewicz <mbazyl2@gmail.com>
2017-04-18 17:27:06 +0200

    Showing products with images

--- browseShop.php
+++ browseShop.php
@@ -2,20 +2,17 @@
 define('TITLE', "Browse Shop;");
 include('structure/header.php');
 
-$image = Image::loadByItemId(3);
-$path = $image->getSource();
-echo $path;
-var_dump($image);
-
-foreach ($image as $value) {
-  $path = $value->getSource();
-  echo $path;
-}
+// $image = Image::loadByItemId(3);
+// $path = $image->getSource();
 
 $items = Item::loadAll();
 // var_dump($items);
     echo "<table class='table'><thead colspan='4'><tr><span class='table-header'>Items</span></th></tr></thead><tr><td><span class='table-col'>Id</span></td><td><span class='table-col'>Name</span></td><td><span class='table-col'>Price</span></td><td><span class='table-col'>Description<span></td><td><span class='table-col'>Image<span></td></tr>";
     foreach ($items as $key) {
+        $itemId = $key->getId();
+        //var_dump($itemId);
+        $images = Image::loadByItemId($itemId);
+        $path = $images->getSource();
         echo "<tr><td>".$key->getId()."</td><td>".$key->getName()."</td><td>".$key->getPrice()."</td><td>".$key->getDescription()."</td><td>"
         ."<img src='$path' width='130' height='80'/>"."</td></tr>";
 
