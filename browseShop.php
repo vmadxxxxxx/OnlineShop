a65bbf6a3cdae801802be1d35cb25f3d69fec40e
Marek Bazylewicz <mbazyl2@gmail.com>
2017-04-18 13:05:27 +0200

    Fixed headers

--- browseShop.php
+++ browseShop.php
@@ -4,7 +4,7 @@ include('structure/header.php');
 
 
 $items = Item::loadAll();
-    echo "<table class='table'><thead colspan='4'><tr><span class='table-header'>Users</span></th></tr></thead><tr><td><span class='table-col'>Id</span></td><td><span class='table-col'>Name</span></td><td><span class='table-col'>Surname</span></td><td><span class='table-col'>E-mail<span></td></tr>";
+    echo "<table class='table'><thead colspan='4'><tr><span class='table-header'>Items</span></th></tr></thead><tr><td><span class='table-col'>Id</span></td><td><span class='table-col'>Name</span></td><td><span class='table-col'>Price</span></td><td><span class='table-col'>Description<span></td></tr>";
     foreach ($items as $key) {
         echo "<tr><td>".$key->getId()."</td><td>".$key->getName()."</td><td>".$key->getPrice()."</td><td>".$key->getDescription()."</td></tr>";
 
