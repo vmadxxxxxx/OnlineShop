358c82e410710df1d2634ce628a77dd254e75eb0
IncredibleQuark <lkupinski@gmail.com>
2017-04-18 20:06:43 +0200

    Item.php small fix. Admin edit item not working

--- resources/api/adminEditItem.php
+++ resources/api/adminEditItem.php
@@ -4,17 +4,17 @@ include($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/structure/header.php');
 // receiving json from adminPanel.js and action after clicking delete User button
     if ($_SERVER['REQUEST_METHOD'] == 'DELETE') {
 
-        
+
         parse_str(file_get_contents("php://input"), $del_vars);
 
-        $idItem = intval($del_vars['id']);
+        $id = intval($del_vars['id']);
 
-        $toDeleteItem = Item::loadById($idItem);
+        $toDelete = Item::loadAll();
 
-        $resultItem = $toDeleteItem->delete($idItem);
+        $resultUser = Item::delete($id);
         
 
-     
+     //not finished, errors with deleting item (deleting user works fine)
 }
 
 include($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/structure/footer.php');
