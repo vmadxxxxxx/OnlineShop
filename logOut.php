3ad01644359d40fbddc4b96df10a02da547e1cbb
IncredibleQuark <lkupinski@gmail.com>
2017-04-18 12:36:06 +0200

    Loggin out admin edited

--- logOut.php
+++ logOut.php
@@ -3,7 +3,7 @@ define('TITLE', "LogIn");
 include('structure/header.php');
 
 unset($_SESSION['email']);
-
+unset($_SESSION['adminEmail']);
 echo "You have benn logged out!";
 
 header("Refresh: 1 index.php?");
