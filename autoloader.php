a03cc2faa3029486288039bfa51a109965a5810d
IncredibleQuark <lkupinski@gmail.com>
2017-04-18 16:18:29 +0200

    Project structure modified. admin.js created

--- autoloader.php
+++ autoloader.php
@@ -3,9 +3,9 @@
 //    require_once($filename);
 //} ten sposob nie dziala jesli importuje plik w folderze wyzej niz ten w ktorym jest index.php
 
-require('./resources/class/activeRecordInterface.php');
-require('./resources/class/activeRecord.php');
-require('./resources/class/User.php');
-require('./resources/class/Admin.php');
-require('./resources/class/Item.php');
-require('./resources/config.php');
+require($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/resources/class/activeRecordInterface.php');
+require($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/resources/class/activeRecord.php');
+require($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/resources/class/User.php');
+require($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/resources/class/Admin.php');
+require($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/resources/class/Item.php');
+require($_SERVER['DOCUMENT_ROOT'].'/OnlineShop/resources/config.php');
--- structure/js/adminPanel.js
+++ structure/js/adminPanel.js
@@ -1,12 +1,36 @@
 $(function () {
-    
-    $delBtns = $('.delBtn')
-    
-    $delBtns.on('click', function () {
-        echo "lipa";
-    })
-    
-    
+
+    var delBtnsUser = $('.btnDelUser');
+
+    delBtnsUser.click(function (e) {
+
+        e.preventDefault();
+        var btn = $(this);
+        
+        var id = btn.parent().siblings().first().text();
+       
+        
+       console.log(id);
+        
+        
+        $.ajax({
+            url: './resources/api/admin.php',
+            dataType: 'json',
+            data: 'id='+id,
+            type: 'DELETE'
+
+        }).done(function () {
+            btn.parent().parent().remove();
+            alert('jes');
+
+        }).fail(function () {
+            alert('chujnia');
+        });
+
+
+    });
+
+
 });
 
 
