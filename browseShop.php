120d83f7fb705bce5b0ad774aa0c77c59e0974b3
IncredibleQuark <lkupinski@gmail.com>
2017-04-27 19:51:12 +0200

    Added validation if user is logged in, if not - redirect

--- browseShop.php
+++ browseShop.php
@@ -1,26 +1,34 @@
 <?php
+
 define('TITLE', "Browse Shop;");
 include('structure/header.php');
 
-$items = Item::loadAll();
+if (isset($_SESSION['id'])) {
+    $items = Item::loadAll();
 
-echo "<table class='table'><thead colspan='4'><tr><span class='table-header'>Items</span></th></tr></thead><tr><td><span class='table-col'>Id</span></td><td><span class='table-col'>Name</span></td><td><span class='table-col'>Price</span></td><td><span class='table-col'>Description<span></td><td class='img'><span class='table-col'>Image<span></td></tr>";
+    echo "<table class='table'><thead colspan='4'><tr><span class='table-header'>Items</span></th></tr></thead><tr><td><span class='table-col'>Id</span></td><td><span class='table-col'>Name</span></td><td><span class='table-col'>Price</span></td><td><span class='table-col'>Description<span></td><td class='img'><span class='table-col'>Image<span></td></tr>";
     foreach ($items as $key) {
         $itemId = $key->getId();
         $pictures = Image::loadAllById($itemId);
 
-        echo "<tr><td>".$key->getId()."</td><td>".$key->getName()."</td><td>".$key->getPrice()."</td><td>".$key->getDescription()."</td>";
+        echo "<tr><td>" . $key->getId() . "</td><td>" . $key->getName() . "</td><td>" . $key->getPrice() . "</td><td>" . $key->getDescription() . "</td>";
         echo "<td>";
-    foreach ($pictures as $val) {
-        $sciezka = $val->getSource();
-        echo "<a href='$sciezka'><img src='$sciezka' width='130' height='80'/></a>";
+        foreach ($pictures as $val) {
+            $sciezka = $val->getSource();
+            echo "<a href='$sciezka'><img src='$sciezka' width='130' height='80'/></a>";
         }
     }
-      echo"</td></tr>";
+    echo"</td></tr>";
     echo "</table>";
+} else {
+    echo "You have to log in first!";
+    header("Refresh: 2 loginForm.php?");
+    echo "<br>Click here if you are not redirected autmatically! <a href='loginForm.php'>Login</a>";
+}
 ?>
 
 
 <?php
+
 include('structure/footer.php');
 ?>
