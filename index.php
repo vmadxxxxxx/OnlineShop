d16b94a52a6c758cb4768f40d089f0e89b144ca0
IncredibleQuark <lkupinski@gmail.com>
2017-04-27 19:52:04 +0200

    Added second carousel and linked Buy Now button

--- index.php
+++ index.php
@@ -1,51 +1,93 @@
 <?php
 define('TITLE', "Main page");
 include('structure/header.php');
+
+$item1 = Item::loadById(1);
+$image1 = Image::loadById(6);
+$image2 = Image::loadById(7);
+$image3 = Image::loadById(8);
 ?>
 
 
-<div class="col-md-6 carousel" id="moja-karuzela" data-ride="carousel">
-    <h1>iPhone 7</h1>
+<div class="col-md-6 carousel" id="karuzela1" data-ride="carousel">
+    <h2>iPhone 7</h2>
     
     <ol class="carousel-indicators">
-        <li data-target="#moja-karuzela" data-slide-to="0" class="active"></li>
-        <li data-target="#moja-karuzela" data-slide-to="1"></li>
-        <li data-target="#moja-karuzela" data-slide-to="2"></li>
+        <li data-target="#karuzela1" data-slide-to="0" class="active"></li>
+        <li data-target="#karuzela1" data-slide-to="1"></li>
+        <li data-target="#karuzela1" data-slide-to="2"></li>
     </ol>
     
     <div class="carousel-inner" role="listbox">
         <div class="item active">
-            <img class="img img-responsive center-block" src="./resources/Images/iphone71.jpg">
+            <img class="img img-responsive center-block" src="<?= $image1->getSource();?>">
         </div>
         <div class="item">
-             <img class="img img-responsive center-block" src="./resources/Images/iphone72.jpg">
+             <img class="img img-responsive center-block" src="<?= $image2->getSource();?>">
         </div>
         <div class="item">
-             <img class="img img-responsive center-block" src="./resources/Images/iphone73.jpg">
+             <img class="img img-responsive center-block" src="<?= $image3->getSource();?>">
         </div>
     
-        <a class="left carousel-control" href="#moja-karuzela" role="button" data-slide="prev">
+        <a class="left carousel-control" href="#karuzela1" role="button" data-slide="prev">
             <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
             <span class="sr-only">Previous</span>
         </a>
-        <a class="right carousel-control" href="#moja-karuzela" role="button" data-slide="next">
+        <a class="right carousel-control" href="#karuzela1" role="button" data-slide="next">
             <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
             <span class="sr-only">Next</span>
         </a>
     </div>    
     
-    <div class="bg-warning">
-        <h3  id="price">Price: <s class="text-muted"> 4199 zł </s> <span class="glyphicon glyphicon-arrow-right"></span>
-        <strong><span> 3199 zł </span></strong><small><a class="text-danger bg-danger" href="#"> Buy now! </a></small></h3>
+    <div class="bg-info">
+        <h4  id="price">Price: <s class="text-muted"> 4199 zł </s> <span class="glyphicon glyphicon-arrow-right"></span>
+        <strong><span> <?= $item1->getPrice();?> zł </span></strong><a class="btn btn-danger" href="browseShop.php"> Buy now! </a></h4>
     </div>
         
 </div>
 
 
+<?php 
+$item2 = Item::loadById(2);
+$image4 = Image::loadById(1);
+$image5 = Image::loadById(2);
+$image6 = Image::loadById(3);
+?>
 
-
-<div class="col-md-6">
-    <p>Przedmiot 2</p>
+<div class="col-md-6 carousel" id="karuzela2" data-ride="carousel">
+    <h2>Samsung Galaxy S7</h2>
+    
+    <ol class="carousel-indicators">
+        <li data-target="#karuzela2" data-slide-to="0" class="active"></li>
+        <li data-target="#karuzela2" data-slide-to="1"></li>
+        <li data-target="#karuzela2" data-slide-to="2"></li>
+    </ol>
+    
+    <div class="carousel-inner" role="listbox">
+        <div class="item active">
+            <img class="img img-responsive center-block" src="<?= $image4->getSource();?>">
+        </div>
+        <div class="item">
+             <img class="img img-responsive center-block" src="<?= $image5->getSource();?>">
+        </div>
+        <div class="item">
+             <img class="img img-responsive center-block" src="<?= $image6->getSource();?>">
+        </div>
+    
+        <a class="left carousel-control" href="#karuzela2" role="button" data-slide="prev">
+            <span class="glyphicon glyphicon-chevron-left" aria-hidden="true"></span>
+            <span class="sr-only">Previous</span>
+        </a>
+        <a class="right carousel-control" href="#karuzela2" role="button" data-slide="next">
+            <span class="glyphicon glyphicon-chevron-right" aria-hidden="true"></span>
+            <span class="sr-only">Next</span>
+        </a>
+    </div>    
+    
+    <div class="bg-info">
+        <h4  id="price">Price: <s class="text-muted"> 3500 zł </s> <span class="glyphicon glyphicon-arrow-right"></span>
+        <strong><span> <?= $item2->getPrice();?> zł </span></strong><a class="btn btn-danger" href="browseShop.php"> Buy now! </a></h4>
+    </div>
 </div>
 
 <?php
