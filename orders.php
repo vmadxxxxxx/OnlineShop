63a234a5e040170458757c672361580d12edb4da
Marek Bazylewicz <mbazyl2@gmail.com>
2017-05-04 19:39:48 +0200

    Starts working on Order Class.

--- orders.php
+++ orders.php
@@ -3,3 +3,6 @@
 define('TITLE', "My orders");
 include('structure/header.php');
 echo "<br> Place for displaying accepted orders <br>";
+
+
+
---
+++ resources/class/Order.php
@@ -0,0 +1,30 @@
+<?php
+
+
+class Order extends activeRecord implements JsonSerializable
+{
+    public function getId()
+        {
+
+        }
+    public function save()
+        {
+
+        }
+    public function delete()
+        {
+
+        }
+    static public function loadAll()
+        {
+
+        }
+    static public function loadById($id)
+        {
+
+        }
+    public function jsonSerialize()
+        {
+
+        }
+}
